b29b1abfea07e12a8fbc41618e79199a789f65be
Jordan Gomes <me@jordangomes.com>
2023-12-29 21:03:19 +1000

    initialize repo

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.GET("/", hello)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "henlo world :)")
+}
